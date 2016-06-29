cc2be73f95673e6ecbbe20efcfa859510cea0898
zaiboot <zaiboot@gmail.com>
2016-06-28 20:27:13 -0600

    Adding changes for restore Virtualbox

--- restore-vm-vbox-manage.ps1
+++ restore-vm-vbox-manage.ps1
@@ -4,3 +4,4 @@ $pathToTheVM=$env:UserProfile+"\Downloads\IE11.Win7.For.Windows.VirtualBox\IE11 
 # unit 4: Sound card "" -- disabled
 & $VBoxManage import $pathToTheVM --vsys 0 --unit 4 --ignore --vmname IE11-Win7v2
 & $VBoxManage modifyvm "IE11-Win7v2" --nic1 bridged --bridgeadapter1 eth0
+#looks like the units of each one are not standardize. Unit 4  could be anything, from sound card to RAM disk
