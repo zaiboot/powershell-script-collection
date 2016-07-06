2d2d5161e0981aff16c48f2c03ec89e9ed448cba
zaiboot <zaiboot@gmail.com>
2016-07-05 22:37:58 -0600

    Adding folder scanner

---
+++ Folder Scanner.ps1
@@ -0,0 +1,10 @@
+﻿clear
+$folderToReview = '~\projects\'
+$patternToInclude = '*.*'
+$howmanyHoursAgo =-6;
+$dateToMatchAgainst = (Get-Date).AddHours($howmanyHoursAgo)
+ls $folderToReview  -Include $patternToInclude -Recurse | 
+    where {
+        $_.LastWriteTime -gt $dateToMatchAgainst.Date ;
+            Write-Progress  -Activity 'Writing records'  -Status "File name: $_";    
+    }  
