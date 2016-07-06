b4dd8d6d9d47dbc2ed002370fd077da4c4b337ea
zaiboot <zaiboot@gmail.com>
2016-07-05 22:15:15 -0600

    Adding changes for vs settings debugging attach warning

--- visual-studio-settings.ps1
+++ visual-studio-settings.ps1
@@ -1,11 +1,5 @@
-function Set-Registry-Value($path, $value, $propertyType)
-{
-    if ( Test-Path -Path $path ){
-                
-    }
-}
-
 #Basic settings
+clear
 $VS_REGISTRY_PATH = "HKCU:\Software\Microsoft\VisualStudio\14.0\"
 $PROJECTS_PATH =  '%USERPROFILE%\projects'
 Set-ItemProperty -path $VS_REGISTRY_PATH  -value $PROJECTS_PATH  -name 'DefaultOpenProjectLocation'
@@ -13,17 +7,7 @@ Set-ItemProperty -path $VS_REGISTRY_PATH  -value $PROJECTS_PATH  -name 'DefaultO
 
 $VS_BASIC_TEXT_EDITOR = "$VS_REGISTRY_PATH\Text Editor\Basic" 
 Set-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 4  -name 'Tab Size'
-Set-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 1  -name 'Line Numbers' #TRUE
+Set-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 1  -name 'Line Numbers' 
 
 $VS_DEBUGGER ="$VS_REGISTRY_PATH\Debugger\"
-New-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 1  -name 'DisableAttachSecurityWarning' -PropertyType DWORD
-
-
-IF ( (Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Length -gt 0 )
-{
-    Write-Host "yeah"
-}else{
-    Write-Host "lenght = " +  (Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Length
-}
-
-(Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Name
+New-ItemProperty -path $VS_DEBUGGER  -value 1  -name 'DisableAttachSecurityWarning' -PropertyType DWORD -Force 
