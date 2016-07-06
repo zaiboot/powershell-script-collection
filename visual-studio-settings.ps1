9d7904b0ce3933afed49edf7513369c9e254dd93
zaiboot <zaiboot@gmail.com>
2016-07-05 22:01:29 -0600

    Adding windows services examples

--- visual-studio-settings.ps1
+++ visual-studio-settings.ps1
@@ -1,5 +1,29 @@
+function Set-Registry-Value($path, $value, $propertyType)
+{
+    if ( Test-Path -Path $path ){
+                
+    }
+}
+
 #Basic settings
 $VS_REGISTRY_PATH = "HKCU:\Software\Microsoft\VisualStudio\14.0\"
 $PROJECTS_PATH =  '%USERPROFILE%\projects'
 Set-ItemProperty -path $VS_REGISTRY_PATH  -value $PROJECTS_PATH  -name 'DefaultOpenProjectLocation'
 Set-ItemProperty -path $VS_REGISTRY_PATH  -value $PROJECTS_PATH  -name 'DefaultOpenSolutionLocation'
+
+$VS_BASIC_TEXT_EDITOR = "$VS_REGISTRY_PATH\Text Editor\Basic" 
+Set-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 4  -name 'Tab Size'
+Set-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 1  -name 'Line Numbers' #TRUE
+
+$VS_DEBUGGER ="$VS_REGISTRY_PATH\Debugger\"
+New-ItemProperty -path $VS_BASIC_TEXT_EDITOR  -value 1  -name 'DisableAttachSecurityWarning' -PropertyType DWORD
+
+
+IF ( (Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Length -gt 0 )
+{
+    Write-Host "yeah"
+}else{
+    Write-Host "lenght = " +  (Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Length
+}
+
+(Get-ItemProperty  -Path 'HKCU:\Software\Microsoft\VisualStudio\14.0\Debugger\'  -Name D*).Name
---
+++ windows-services.ps1
@@ -0,0 +1,14 @@
+﻿#windows-services.ps1
+Get-Service # list all of the services.
+
+Get-Service w* #Search services by name
+
+# very obvious
+Start-Service  -Name Tomcat 
+Stop-Service
+Restart-Service
+
+# service management
+New-Service 
+(Get-WmiObject win32_service -Filter "name='TestService'").delete()  #delete a service
+Set-Service
