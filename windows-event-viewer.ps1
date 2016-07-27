913413ee253d50bab2bae2d1e119e8e918afcd4b
zaiboot <zaiboot@gmail.com>
2016-07-27 09:19:34 -0600

    Adding several script changes

--- windows-event-viewer.ps1
+++ windows-event-viewer.ps1
@@ -1,1 +1,7 @@
 ﻿Get-WinEvent -FilterHashtable @{ProviderName='nssm';logname='Application'} -MaxEvents 10 | ft -wrap
+
+
+$dev6Information =  $(Get-WmiObject win32_service | ?{$_.Name -like '*Intellicus*P*Dev6'} | SELECT *)
+$dev20_sp1Information = $(Get-WmiObject win32_service -Verbose | ?{$_.Name -like '*Intellicus*P*Dev20_2'} | SELECT *)
+
+Compare-Object -ReferenceObject $dev6Information[0]  -DifferenceObject $dev20_sp1Information[0]
---
+++ xml-parsing.ps1
@@ -0,0 +1,6 @@
+﻿clear
+cd ~
+#xml parsing
+$fileName = '~\Dropbox (Trintech)\Intellicus\reporting_roles_fragment_for_integration.xml'
+$xPathExpression = "/root/REPORTING_ROLE/ENTITY_TYPES/ENTITY_TYPE/ENTITY/@ID"
+Select-Xml -Path $fileName -XPath  $xPathExpression |  % {"('"+ $_.Node."#text" +"')," } 
