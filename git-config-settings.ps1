68257a53a67a43982c057c816c802a978753f4e6
Edgar Madrigal <zaiboot@users.noreply.github.com>
2016-07-28 19:26:20 -0600

    Update git-config-settings.ps1

--- git-config-settings.ps1
+++ git-config-settings.ps1
@@ -1,5 +1,9 @@
-﻿git config user.name zaiboot
+git config user.name zaiboot
 git config user.email zaiboot@gmail.com
+
+git config --global user.name zaiboot
+git config --global user.email zaiboot@gmail.com
+
 git config --global --add merge.tool kdiff3
 git config --global --add mergetool.kdiff3.path "C:/Program Files/KDiff3/kdiff3.exe"
 git config --global --add mergetool.kdiff3.trustExitCode false
