7a5a41d79b0dc3506e85f66fa2d2cf9262de87b6
zaiboot <zaiboot@gmail.com>
2016-11-01 15:49:10 -0600

    Adding misc files

---
+++ Restart-Windows-Service-Remote.ps1
@@ -0,0 +1,11 @@
+﻿#Restart-Windows-Service remote
+clear
+
+Invoke-Command -ComputerName "dalqahvs82"  -ScriptBlock {
+    #Import-Module WebAdministration
+    #Get-ChildItem –Path IIS:\AppPools\*qa82-280*  | Get-Member
+    #$env:Path += ';' + $env:windir +'\system32\inetsrv\'
+    #appcmd list wps
+    Get-Service -DisplayName '*2015*'
+    Restart-Service -DisplayName '*2015*'
+}
