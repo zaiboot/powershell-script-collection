9e4660ae17d4830a69a83e384643bfa604a9b393
Edgar Madrigal <zaiboot@users.noreply.github.com>
2017-05-05 17:10:51 -0600

    Update git-config-settings.ps1

--- git-config-settings.ps1
+++ git-config-settings.ps1
@@ -3,6 +3,7 @@ git config user.email zaiboot@gmail.com
 git config --global push.default simple
 git config --global user.name zaiboot
 git config --global user.email zaiboot@gmail.com
+git config --global pull.rebase true
 
 git config --global --add merge.tool kdiff3
 git config --global --add mergetool.kdiff3.path "C:/Program Files/KDiff3/kdiff3.exe"
